208a69ecf62bc83ed3039eac3f8a0daa173c955d
Jakub Neruda <xnerud01@stud.fit.vutbr.cz>
2019-03-16 12:01:25 +0100

    [DOCS] CommodityPriceCounter

--- src/datatypes/CommodityPriceCounter.java
+++ src/datatypes/CommodityPriceCounter.java
@@ -1,7 +1,7 @@
 package datatypes;
 
 /**
- *  @brief How commodity is prices
+ *  @brief How commodity is priced (by some amount of time or per item)
  */
 public enum CommodityPriceCounter {
 	SECOND,
