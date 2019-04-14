d3d926bf155c13238a7f13b2e487eeb297edb30f
Ondra <sedloon@seznam.cz>
2019-04-14 19:04:58 +0200

    login and logout

--- src/back/UserBean.java
+++ src/back/UserBean.java
@@ -1,17 +1,17 @@
 package back;
 
-import java.io.IOException;
 import java.io.Serializable;
 
 import java.util.List;
 
 import javax.faces.application.FacesMessage;
-import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 
 import core.User;
+import datatypes.UserRole;
+
 import datatypes.UserRole;
 
 @Named
@@ -65,10 +65,116 @@ public class UserBean implements Serializable {
 	String lastName;
     String email;
     String password;
+    
+    Boolean isVisitor = true;
+    Boolean isRegistrated = false;
+    Boolean isReceptionist = false;
+    Boolean isManager = false;
+    Boolean isAdmin = false;
+        
+    core.User user;
+        
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // Public functions
+        
+    public UserBean() {
+	}
+    
+    // logout
+    public void actionLogout() {
+    	user = null;
+    	updateRoleIndicators(UserRole.VISITOR);
+    }
+    
+	// login
+	public String actionLogin() {
+		try {
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("[DEBUG] email: " + email + " password: " + password));
+			
+			user = new core.User(email, password);
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Login successful"));
+			updateRoleIndicators(user.getRole());
+			
+			//FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("[DEBUG] UserRole.MANAGER: " + UserRole.MANAGER));
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("[DEBUG] user.getRole(): " + user.getRole()));
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("[DEBUG] isVisitor: " + isVisitor));
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("[DEBUG] isRegistrated: " + isRegistrated));
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("[DEBUG] isReceptionist: " + isReceptionist));
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("[DEBUG] isManager: " + isManager));
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("[DEBUG] isAdmin: " + isAdmin));
+		} catch (Exception e) {
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(e.getMessage()));
+			return "loginFailed";
+		}
+		
+		return "loginSuccessful";
+	}
+	
+	// register
+	public String actionRegister() {
+		try {
+			user = new core.User(firstName, lastName, email, password);
+		} catch (Exception e) {
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(e.getMessage()));
+		}
+		
+		return "register";
+	}
+	
+	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	// Private functions
+	
+	private void updateRoleIndicators(UserRole role) {
+    	isVisitor = isRegistrated = isReceptionist = isManager = isAdmin = false;
+    	switch (role) {
+    	case REGISTRATED:
+    		isRegistrated = true;
+    		break;
+    	case RECEPTIONIST:
+    		isReceptionist = true;
+    		break;
+    	case MANAGER:
+    		isManager = true;
+    		break;
+    	case ADMIN:
+    		isAdmin = true;
+    		break;
+    	default:
+    		isVisitor = true;
+    	}
+    }
+    
+	
+	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	// Getters and Setters
 
+	public Boolean getIsVisitor() {
+		return isVisitor;
+	}
+
+	public Boolean getIsRegistrated() {
+		return isRegistrated;
+	}
+
+	public Boolean getIsReceptionist() {
+		return isReceptionist;
+	}
+
+	public Boolean getIsManager() {
+		return isManager;
+	}
+
+	public Boolean getIsAdmin() {
+		return isAdmin;
+	}
+	
     public String getFirstName() {
 		return firstName;
 	}
+
+    
+    
+    
 
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
@@ -96,38 +202,5 @@ public class UserBean implements Serializable {
 
 	public void setPassword(String password) {
 		this.password = password;
-	}
-
-	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-	public void redirect() throws IOException {
-	    ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
-	    externalContext.redirect("http://stackoverflow.com");
-	}
-	
-	// login
-	public String actionLogin() {
-		try {
-			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("[DEBUG] email: " + email + " password: " + password));
-			new core.User(email, password);
-			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Login successful"));
-		} catch (Exception e) {
-			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(e.getMessage()));
-			//e.printStackTrace();
-			return "loginFailed";
-		}
-		
-		return "loginSuccessful";
-	}
-	
-	// register
-	public String actionRegister() {
-		try {
-			new core.User(firstName, lastName, email, password);
-		} catch (Exception e) {
-			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(e.getMessage()));
-		}
-		
-		return "register";
 	}
 }
